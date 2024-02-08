3f1f9b3020659e6a5646dab71a0a423866af9c1d
lifeofdmt <dman2litty@gmail.com>
2024-02-07 22:18:27 -0500

    feat: Define model and default network parameters

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-from torchvision import datasets, transforms
-import torch
+from torchvision import datasets, transforms, models
+from torch import nn, optim, utils, cuda
 
 # Define dataset transforms
 train_transform = transforms.Compose([transforms.Resize(256), transforms.RandomResizedCrop(224), transforms.RandomRotation(30),
@@ -13,9 +13,33 @@ image_datasets = [datasets.GTSRB(root='GTSRB/train', download=True, split='train
                   datasets.GTSRB(root='GTSRB/test', download=True, split='test', transform=test_transform)]
 
 
-print(image_datasets[0])
-print(image_datasets[1])
+# Download vgg19 model for transfer learning
+model = models.vgg19(pretrained=True) # inputs: 25088, outputs: 43
 
-# Create dataloaders
-dataloaders = [torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True) for dataset in image_datasets]
-print(dataloaders)
+# Freeze network parameters
+for param in model.parameters():
+  param.requires_grad = False
+
+# Create classifier 
+dropout_rate = 0.3
+
+classifier = nn.Sequential(nn.Linear(in_features=25088, out_features=1024),
+                            nn.ReLU(),
+                            nn.Dropout(p=dropout_rate),
+                            nn.Linear(in_features=1024, out_features=512),
+                            nn.ReLU(),
+                            nn.Dropout(p=dropout_rate),
+                            nn.Linear(in_features=512, out_features=256),
+                            nn.ReLU(),
+                            nn.Dropout(p=dropout_rate),
+                            nn.Linear(in_features=256, out_features=43),
+                            nn.LogSoftmax(dim=1))
+
+# Replace model's classifier
+model.classifier = classifier
+
+# Define training parameters
+learning_rate = 0.001
+optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
+
+
